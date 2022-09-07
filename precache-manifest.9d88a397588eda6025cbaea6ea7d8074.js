0379dfae07d801cb7d2c663149c789ea721e83ba
shaniceng <ngshanice1@yahoo.com.sg>
2022-09-07 08:21:07 +0800

    Updates

---
+++ precache-manifest.9d88a397588eda6025cbaea6ea7d8074.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ec15a759f45e2d8d64d365082af676",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "3084e018aff34468f84b",
+    "url": "/my-website/static/css/2.77121a78.chunk.css"
+  },
+  {
+    "revision": "d3a2fd9f73ca71b9f31e",
+    "url": "/my-website/static/css/main.a1c58eaf.chunk.css"
+  },
+  {
+    "revision": "3084e018aff34468f84b",
+    "url": "/my-website/static/js/2.9fda5789.chunk.js"
+  },
+  {
+    "revision": "d3a2fd9f73ca71b9f31e",
+    "url": "/my-website/static/js/main.81b75917.chunk.js"
+  },
+  {
+    "revision": "b69a60d4bdd37d05f45c",
+    "url": "/my-website/static/js/runtime-main.166fa9b2.js"
+  },
+  {
+    "revision": "b47e7e67058525ffdbe7f1a447f6bdcd",
+    "url": "/my-website/static/media/about.b47e7e67.png"
+  },
+  {
+    "revision": "c9fbc06bd04b45e28d61528d5984c105",
+    "url": "/my-website/static/media/headerBackground.c9fbc06b.jpg"
+  },
+  {
+    "revision": "65877fb9b3af4fb58970a061cd69b080",
+    "url": "/my-website/static/media/pdf1.65877fb9.pdf"
+  }
+]);
