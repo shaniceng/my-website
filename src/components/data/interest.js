38cf464769040d22efcb98d371bf01ff1f92d805
shaniceng <ngshanice1@yahoo.com.sg>
2024-02-05 11:50:13 +0800

    update images

--- src/components/data/interest.js
+++ src/components/data/interest.js
@@ -6,28 +6,28 @@ import Music from "@material-ui/icons/MusicNote";
 
 const interest = [
 	{
-		src: "/images/volleyball.png",
+		src: process.env.PUBLIC_URL + "/images/volleyball.jpg",
 		alt: "Girl playing volleyball",
 		icon: <Volleyball fontSize="large" />,
 		title: "Play Volleyball",
 		detail: "Most favourite sports to play at my free time",
 	},
 	{
-		src: "/images/crochet.png",
+		src: process.env.PUBLIC_URL + "/images/crochet.jpg",
 		alt: "Crochet",
 		icon: <Favourite fontSize="large" />,
 		title: "Crochet",
 		detail: "Love crocheting as a form to relief stress",
 	},
 	{
-		src: "/images/music.png",
+		src: process.env.PUBLIC_URL + "/images/music.jpg",
 		alt: "Listen to Music",
 		icon: <Music fontSize="large" />,
 		title: "Listening to Music",
 		detail: "Love listening to mandopop and cantopop",
 	},
 	{
-		src: "/images/singing.png",
+		src: process.env.PUBLIC_URL + "/images/singing.jpg",
 		alt: "Singing",
 		icon: <Mic fontSize="large" />,
 		title: "Singing",
